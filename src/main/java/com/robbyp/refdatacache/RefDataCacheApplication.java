3e0d78231a9db4dd5d709a5175094c9519d8ba2d
Rob Purcell <rob.purcell@gmail.com>
2017-09-26 06:54:09 +0100

    Initial commit

---
+++ src/main/java/com/robbyp/refdatacache/RefDataCacheApplication.java
@@ -0,0 +1,12 @@
+package com.robbyp.refdatacache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefDataCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefDataCacheApplication.class, args);
+	}
+}
